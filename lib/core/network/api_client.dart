1458a446493bc056c2fdb1dcad2baf3d0a84e6c9
Abu Ubaida <ubaidanazia@gmail.com>
2025-11-18 16:50:57 +0600

    Add debug logging for API requests and responses in api_client

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'network_config.dart';
 
@@ -33,7 +34,49 @@ class ApiClient {
 
   Future<dynamic> get(String path, {Map<String, String>? queryParameters}) async {
     final uri = _buildUri(path, queryParameters);
+    if (kDebugMode) {
+      print('API GET Request: $uri');
+    }
     final response = await _http.get(uri, headers: _defaultHeaders()).timeout(timeout);
+    if (kDebugMode) {
+      print('API GET Response: Status ${response.statusCode}, Body: ${response.body}');
+    }
+    return _processResponse(response);
+  }
+
+  Future<dynamic> post(String path, {Map<String, dynamic>? body, Map<String, String>? queryParameters}) async {
+    final uri = _buildUri(path, queryParameters);
+    if (kDebugMode) {
+      print('API POST Request: $uri, Body: $body');
+    }
+    final response = await _http.post(uri, headers: _defaultHeaders(), body: jsonEncode(body)).timeout(timeout);
+    if (kDebugMode) {
+      print('API POST Response: Status ${response.statusCode}, Body: ${response.body}');
+    }
+    return _processResponse(response);
+  }
+
+  Future<dynamic> put(String path, {Map<String, dynamic>? body, Map<String, String>? queryParameters}) async {
+    final uri = _buildUri(path, queryParameters);
+    if (kDebugMode) {
+      print('API PUT Request: $uri, Body: $body');
+    }
+    final response = await _http.put(uri, headers: _defaultHeaders(), body: jsonEncode(body)).timeout(timeout);
+    if (kDebugMode) {
+      print('API PUT Response: Status ${response.statusCode}, Body: ${response.body}');
+    }
+    return _processResponse(response);
+  }
+
+  Future<dynamic> delete(String path, {Map<String, String>? queryParameters}) async {
+    final uri = _buildUri(path, queryParameters);
+    if (kDebugMode) {
+      print('API DELETE Request: $uri');
+    }
+    final response = await _http.delete(uri, headers: _defaultHeaders()).timeout(timeout);
+    if (kDebugMode) {
+      print('API DELETE Response: Status ${response.statusCode}, Body: ${response.body}');
+    }
     return _processResponse(response);
   }
 
