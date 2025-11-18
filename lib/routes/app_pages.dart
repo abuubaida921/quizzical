3412d728ac38cad2003244ce4cb67b72dfa6140c
Abu Ubaida <ubaidanazia@gmail.com>
2025-11-18 16:41:01 +0600

    Add bindings for category and quiz controllers in app routing

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
 import 'package:quizzical/features/categories/presentation/pages/category_page.dart';
+import '../features/categories/data/datasources/categories_trivia_remote_data_source.dart';
+import '../features/categories/presentation/controllers/category_controller.dart';
+import '../features/quiz/data/datasources/quiz_trivia_remote_data_source.dart';
+import '../features/quiz/presentation/controllers/quiz_controller.dart';
 import '../features/quiz/presentation/pages/quiz_config_page.dart';
 import '../features/quiz/presentation/pages/quiz_play_page.dart';
 import '../features/splash/presentation/pages/splash_page.dart';
@@ -15,8 +19,34 @@ class AppPages {
   static final routes = [
     GetPage(name: AppPages.splash, page: () => const SplashPage()),
     GetPage(name: AppPages.welcome, page: () => const WelcomePage()),
-    GetPage(name: AppPages.categories, page: () => const CategoryPage()),
-    GetPage(name: AppPages.quizConfigPage, page: () => const QuizConfigPage()),
+    GetPage(
+      name: AppPages.categories,
+      page: () => const CategoryPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<CategoriesTriviaRemoteDataSource>(
+          () => CategoriesTriviaRemoteDataSource(Get.find()),
+          fenix: true,
+        );
+        Get.lazyPut<CategoryController>(
+          () => CategoryController(Get.find()),
+          fenix: true,
+        );
+      }),
+    ),
+    GetPage(
+      name: AppPages.quizConfigPage,
+      page: () => const QuizConfigPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<QuizTriviaRemoteDataSource>(
+          () => QuizTriviaRemoteDataSource(Get.find()),
+          fenix: true,
+        );
+        Get.lazyPut<QuizController>(
+          () => QuizController(Get.find()),
+          fenix: true,
+        );
+      }),
+    ),
     GetPage(name: AppPages.quizPlayPage, page: () => const QuizPlayPage()),
   ];
 }
